1cd8e3d97fb15efea02d1af9b6ffccfb999d3f29
will <william.tjang@blowfishstudios.com>
2022-10-06 17:00:12 +1100

    Initial commit

---
+++ Source/MyUnrealLibrary/MyUnrealLibrary.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyUnrealLibrary.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyUnrealLibrary, "MyUnrealLibrary" );
---
+++ Source/MyUnrealLibrary/MyUnrealLibraryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyUnrealLibraryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUNREALLIBRARY_API AMyUnrealLibraryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
