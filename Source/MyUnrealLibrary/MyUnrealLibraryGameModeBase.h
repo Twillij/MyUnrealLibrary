f3ded3a5ab75bb314211aa617780ff735c006e4f
will <william.tjang@blowfishstudios.com>
2022-10-06 17:29:19 +1100

    Removed copyright comments from source

--- Source/MyUnrealLibrary/MyUnrealLibraryGameModeBase.h
+++ Source/MyUnrealLibrary/MyUnrealLibraryGameModeBase.h
@@ -1,14 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "MyUnrealLibraryGameModeBase.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class MYUNREALLIBRARY_API AMyUnrealLibraryGameModeBase : public AGameModeBase
 {
